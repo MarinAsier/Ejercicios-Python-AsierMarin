338c305b1592782baf9722bff360edaceeff67c2
MarinAsier <asimarsae@alu.edu.gva.es>
2025-03-10 13:13:32 +0100

    Update ejercicio7.py

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__Author__="Asier Marin Saez"
 
 
 # Función que determina si un numero es primo.
@@ -7,25 +7,18 @@ __Author__="José Gaspar Sánchez García"
 def fibonacci(n) :
     vector = []
 
-    if n<1 :
+    if n < 1 :
         return vector
-    elif n==1 :
-        vector.append(1)
-        return vector
-    elif n >=2 :
-        # Implementa las series de Fibonacci
-        # vector[0]=1
-        # vector[1]=1
+    elif n == 1 :
+        return [1]
 
-        vector.append(1)
-        vector.append(1)
-
-        # --> Implemente el código del Bucle <--
+    vector.append(1)
+    vector.append(1)
         
-    elif n==1 :
-        vector[0]=1
+    for i in range(2,n) :
+        vector.append(vector[i-1] + vector[i-2])
 
-    return vector; # Retorno de la función
+    return vector
 
 # Programa principal
 def main():
