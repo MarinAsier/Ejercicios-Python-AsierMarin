129f71867482e1f97698d26b4dacb0c44524ab78
MarinAsier <asimarsae@alu.edu.gva.es>
2025-03-10 12:16:59 +0100

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+#Autor: "Asier Marin Saez"
 
 """Pide una nota (número). Muestra la calificación según la nota:
     0-3: Muy deficiente.
@@ -15,11 +15,23 @@ __Author__="José Gaspar Sánchez García"
 def obtenerCalificaion(nota) :
     calificacion="Incorrecta"
    # Implemente aquí ... Si (condición) entonces ... sino ... si (condición) entonces ... sino ...
+    if nota>=0 and nota<=3:
+       calificacion="Muy deficiente"
+    elif nota>3 and nota<=5:
+        calificacion="Insuficiente"
+    elif nota>5 and nota<=6:
+        calificacion="Suficiente"
+    elif nota>6 and nota<=7:
+        calificacion="Bien"
+    elif nota>7 and nota<9:
+        calificacion="Notable"
+    else:
+        calificacion="Sobresaliente"
     return calificacion
 
 # Programa principal
 def main():
-    n=int(input("Introduzca la nota: "));
-    print('Calificación: '+obtenerCalificaion(n));
+    n=int(input("Introduzca la nota: "))
+    print('Calificación: '+obtenerCalificaion(n))
 if __name__== "__main__" :
    main()
