15cf9b49246f6049186b732e57fb77d7c3cd92c7
MarinAsier <asimarsae@alu.edu.gva.es>
2025-03-10 12:10:27 +0100

    Update ejercicio2b.py

--- ejercicio2b.py
+++ ejercicio2b.py
@@ -1,13 +1,17 @@
 # coding=utf-8
 #Pide la edad y el nombre por teclado. En caso de ser mayor de 18 años indica que ya puede conducir.
-
-# Implemente función esMayorEdad(e)
-
+def mayor_edad(e):
+    return e>=18
 
 # Programa principal
 def main():
-    nombre="Mariano";
-    edad=0;
+    nombre=input("Introduce tu nombre: ")
+    edad=int(input("Introduce tu edad: "))
+
+    if mayor_edad(edad):
+        print(nombre,"ya puede conducir")
+    else:
+        print(nombre,"no puede conducir")
 
     # Utilice la función definida
     # Estructura alternativa Si (condidición con función) entonces --> sino ...
