7c7e798d3fb0f667faa4f77f4444057c189a6d7c
MarinAsier <asimarsae@alu.edu.gva.es>
2025-03-10 13:40:31 +0100

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,17 +1,19 @@
 # coding=utf-8
-__Author__="José Gaspar Sánchez García"
+__Author__="Asier Marin Saez"
 
 def suma(x,y) :
-    return x
+    return x + y
 
 def resta(x,y) :
-    return x
+    return x - y
 
 def multiplica(x,y) :
-    return x
+    return x * y
 
 def divide(x,y) :
-    return y
+    if y == 0 :
+        print("Error: No se puede dividir por cero.")
+    return x / y
 
 # Función que crea el menú de la aplicacion.
 
@@ -31,14 +33,28 @@ def menuApp() :
         if opcion == 1 :
             s1=int(input("Introduzca el primer sumando: "))
             s2=int(input("Introduzca el segundo sumando: "))
-            print("La suma de {0} + {1} = {2}.".format(s1,s2,suma(s1,s2)))
+            print("La suma de {0} + {1} = {2}.\n".format(s1,s2,suma(s1,s2)))
+
         elif opcion == 2 :
             minuendo=int(input("Introduzca el minuendo: "))
             sustraendo=int(input("Introduzca el sustraendo: "))
-            print("La resta de {0} - {1} = {2}.".format(minuendo,sustraendo,resta(minuendo,sustraendo)))
-       # --> Completa el código fuente del programa <-- 
-        elif opcion !=0 :
-            print("Error: Opción incorrecta, introduzca una nueva opción.")
+            print("La resta de {0} - {1} = {2}.\n".format(minuendo,sustraendo,resta(minuendo,sustraendo)))
+
+        elif opcion == 3 :
+            m1 = float(input("Introduzca el primer factor: "))
+            m2 = float(input("Introduzca el segundo factor: "))
+            print("La multiplicación de {0} * {1} = {2}.\n".format(m1,m2,multiplica(m1,m2)))
+
+        elif opcion == 4 :
+            d1 = float(input("Introduzca el dividendo: "))
+            d2 = float(input("Introduzca el divisor: "))
+            print("La división de {0} / {1} = {2}.\n".format(d1,d2,divide(d1,d2)))
+
+        elif opcion == 0 :
+            print("Saliendo de la aplicación...")
+            break
+        else :
+            print("\nError: Opción incorrecta, introduzca una nueva opción.")
 
 # Programa principal
 def main():
