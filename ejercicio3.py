6d1184b59ad7f01ba5ca61e3cc7be52e9e72a12b
MarinAsier <asimarsae@alu.edu.gva.es>
2025-03-11 12:57:51 +0100

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -15,17 +15,17 @@
 def obtenerCalificaion(nota) :
     calificacion="Incorrecta"
    # Implemente aquí ... Si (condición) entonces ... sino ... si (condición) entonces ... sino ...
-    if nota>=0 and nota<=3:
+    if nota>=0 and nota<3:
        calificacion="Muy deficiente"
-    elif nota>3 and nota<=5:
+    elif nota>=3 and nota<5:
         calificacion="Insuficiente"
-    elif nota>5 and nota<=6:
+    elif nota == 5:
         calificacion="Suficiente"
-    elif nota>6 and nota<=7:
+    elif nota == 6:
         calificacion="Bien"
-    elif nota>7 and nota<9:
+    elif nota>=7 and nota<9:
         calificacion="Notable"
-    else:
+    elif nota>=9 and nota<=10:
         calificacion="Sobresaliente"
     return calificacion
 
